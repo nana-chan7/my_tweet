37dfe457aaea13bd7740358012fcfd5355662fe8
frogBear_7 <{220098tt@yse-c.net}>
2023-10-27 13:57:45 +0900

    up

---
+++ regist/result.php
@@ -0,0 +1,28 @@
+<?php
+// 2023-10-27 3.week6
+
+//TODO：セッションチェック
+session_start();
+session_regenerate_id(true);
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include('../components/head.php'); ?>
+
+<body>
+    <?php include('../components/nav.php'); ?>
+
+    <div class="container">
+        <h2>会員登録完了</h2>
+        <p>
+            登録ありがとうございました。
+        </p>
+        <div class="mt-3">
+            <a class="w-100 btn btn-outline-primary" href="../login/">ログイン</a>
+        </div>
+    </div>
+</body>
+
+</html>
